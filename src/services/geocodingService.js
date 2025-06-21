e07148ea7176bfd9e1aab2db98e0905ac3b35331
Satyam . <satyam.student.min21@itbhu.ac.in>
2025-06-21 16:04:28 +0530

    Updated .gitignore file to hide security and envionment related things from the github

--- src/services/geocodingService.js
+++ src/services/geocodingService.js
@@ -1,6 +1,6 @@
 const axios = require('axios');
 
-const GEOAPIFY_API_KEY = '3fc6f043f59d4ae8b550d94ba682e1cb';
+const GEOAPIFY_API_KEY = process.env.GEOAPIFY_API_KEY;
 const GEOAPIFY_API_URL = 'https://api.geoapify.com/v1/geocode/search';
 
 exports.geocodeLocation = async (locationName) => {
